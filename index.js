9eba13139830e4399dbf3b6a5e357db083fee4dd
AbbyKuo <abbykuo8776@gmail.com>
2024-08-08 15:37:24 +1000

    changed the title to display the winner

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
 
 // change the 2nd image src according to the random number generated 
 document.getElementsByClassName("img2")[0].src="/images/dice" +randomNumber2+ ".png";
+
+// Display the winner
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "🚩 Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "Player 2 Wins! 🚩"
+} else {
+    document.querySelector(".container h1").innerHTML = "Draw!"
+};
